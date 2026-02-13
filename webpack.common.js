2b05054f2bead875679d9dbb8e04a47ba1ff8563
lou-bou <boussaid.louai25@gmail.com>
2026-02-13 03:27:40 +0100

    Setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
