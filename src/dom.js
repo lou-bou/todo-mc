d205933bb9cb0db9c333fa2643b6a2728432bc29
lou-bou <boussaid.louai25@gmail.com>
2026-02-16 00:04:04 +0100

    Display task's data in its edit form input fields
    When a user opens the edit task dialog form, it display's that task's
    data pre-filled in the edit form.
    
    Changes:
    - Function to clear a form (empty title field and unchecked checkboxes)
    gets called on a dialog form whenever it's closed
    - Function to clear all forms, gets called every time the page loads
    - Logic for displaying task's data in its edit form
    - Logic for opening dialog edit form and clearing it upon closing

--- src/dom.js
+++ src/dom.js
@@ -8,6 +8,23 @@ function renderTasksDOM() {
     }
 }
 
+function clearForms() {
+    const forms = document.querySelectorAll('.task-form');
+    
+    forms.forEach((form) => {
+        clearForm(form);
+    })
+}
+
+function clearForm(form) {
+    form.title.value = '';
+    form.Personal.checked = false;
+    form.Work.checked = false;
+    form.School.checked = false;
+    form.Urgent.checked = false;
+    form.Optional.checked = false;
+}
+
 function createTaskDOM(taskObject) {
     const tasksContainer = document.querySelector('#tasks');
     const taskContainer = document.createElement('div');
@@ -43,7 +60,11 @@ function handleFormData(form) {
     return {title, categories};
 }
 
-renderTasksDOM(); // gets called each time the page is loaded
+renderTasksDOM(); // gets called each time the page is loaded to display all tasks in localStorage
+clearForms(); // gets called each time the page is loaded to clear all forms from old values
+
+
+// adding a task
 
 const addTaskButton = document.querySelector('#add-task-button');
 const addTaskDialog = document.querySelector('#add-task-dialog');
@@ -64,6 +85,40 @@ addTaskForm.addEventListener('submit', (e) => {
 
     createTaskDOM(task);
 
+    clearForm(addTaskForm);
+
     addTaskDialog.close();
 });
 
+// editing a task
+const editTaskDialog = document.querySelector('#edit-task-dialog');
+const editTaskForm = document.querySelector('#edit-task-form');
+let editTaskButtons; // these buttons may not exist if no tasks are created yet
+if (document.querySelector('.edit-task-button')) {
+    editTaskButtons = document.querySelectorAll('.edit-task-button');
+}
+
+editTaskButtons.forEach((editTaskButton) => {
+    editTaskButton.addEventListener('click', () => {
+        editTaskDialog.showModal();
+
+        const taskId = editTaskButton.getAttribute('data-task-id');
+        const taskObject = PersistanceManager.retrieveTask(taskId);
+
+        editTaskForm.title.value = taskObject.title;
+
+        let editTaskCheckbox; // used to assign each category value looped through
+        
+        for (let i = 0; i < taskObject.categories.length; i++) {
+            editTaskCheckbox = document.querySelector(`#edit-${taskObject.categories[i]}`);
+            editTaskCheckbox.checked = true;
+        }
+    });
+});
+
+editTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    clearForm(editTaskForm);
+    editTaskDialog.close();
+});
